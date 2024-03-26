45c2f1d367956aae595647af2c50446caccf5914
Dylen Gitaka <dylen.gitaka@student.moringaschool.com>
2024-03-26 11:25:29 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42);
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264; // 1 block = 264 feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
+module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice
+};
